a2e91b388236154691733479c6c4189c5fa4e176
Eugen Stan <eugen.stan@netdava.com>
2020-12-05 11:27:45 +0200

    Up to creating functions

--- src/koans/07_functions.clj
+++ src/koans/07_functions.clj
@@ -17,28 +17,30 @@
   (= 10 ((fn [n] (* 5 n)) 2))
 
   "Or using an even shorter syntax"
-  (= __ (#(* 15 %) 4))
+  (= 60 (#(* 15 %) 4))
 
   "Even anonymous functions may take multiple arguments"
-  (= __ (#(+ %1 %2 %3) 4 5 6))
+  (= 15 (#(+ %1 %2 %3) 4 5 6))
 
   "Arguments can also be skipped"
-  (= __ (#(str "AA" %2) "bb" "CC"))
+  (= "AACC" (#(str "AA" %2) "bb" "CC"))
 
   "One function can beget another"
-  (= 9 (((fn [] ___)) 4 5))
+  (= 9 (((fn [] +)) 4 5))
 
   "Functions can also take other functions as input"
   (= 20 ((fn [f] (f 4 5))
-           ___))
+           *))
 
   "Higher-order functions take function arguments"
-  (= 25 (___
+  (= 25 ((fn [ff] (ff 5))
           (fn [n] (* n n))))
 
   "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+  (= 25 ((fn [ff] (ff 5)) square)))
 
 (comment
 
+  ((fn [n] (* n n)))
+  
   0)
